f80f38814d841f6016260fad144b8b03f5834b1d
Srajan <srajanjain1988@gmail.com>
2019-03-15 01:25:59 +0530

    added assertion

--- src/test/java/testClass.java
+++ src/test/java/testClass.java
@@ -2,6 +2,7 @@ import WebLibraries.BrowserClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import Listeners.*;
@@ -16,6 +17,8 @@ public class testClass {
         WebdriverEventListeners eventListeners = new WebdriverEventListeners();
         eventFiringWebDriver.register(eventListeners);
         eventFiringWebDriver.get("http://google.com");
+        String webPageTitle=eventFiringWebDriver.getTitle();
+        Assert.assertEquals(webPageTitle.length()>0,true);
         //System.out.println(driver.manage().logs().get(LogType.PERFORMANCE).getAll().get(0));
 
     }
